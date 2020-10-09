890833fae89e80a202b9c4d18746042bf610ba92
ejveli <71405672+ejveli@users.noreply.github.com>
2020-10-09 20:25:39 +0100

    Add files via upload

--- Question_6.java
+++ Question_6.java
@@ -4,37 +4,26 @@ public class Question_6 {
 
 	public static void main(String[] args) {
 		/*
-		 Write a Java program to print the following pattern
-
-1
-2 6
-3 7 10
-4 8 11 13
-5 9 12 14 15
-
+		 * Write a Java program to print the following pattern
+		 * 
+		 * 1 2 6 3 7 10 4 8 11 13 5 9 12 14 15
+		 * 
 		 */
+
 		
-		
-		int sum=0;
 
 		for (int i = 1; i <= 5; i++) {
-			System.out.print(i + " ");
+			int k=i;
 
-			for (int j = 1;  j< i; j++) { 
-				for(int k=4;j<i;k--) {
-				sum=i+k;	
-				}
-				
-				System.out.print(sum+" ");
-					
-				}System.out.println();
-			
-				}
-			
-				
-				}
-				
+			for (int j = 1; j <= i; j++) 
+			{
+				System.out.print(k+" ");
+				k=k+5-j;
 			}
-		
-	
+			System.out.println();
 
+		}
+
+	}
+
+}
